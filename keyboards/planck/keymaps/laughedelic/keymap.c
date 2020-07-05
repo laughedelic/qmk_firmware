473932a02c4901824715e8ba78ac8b0212f17a3f
Alexey Alekhin <alexey.alekhin@tapad.com>
2020-07-05 22:36:48 +0200

    planck: update to the same as corne

--- keyboards/planck/keymaps/laughedelic/keymap.c
+++ keyboards/planck/keymaps/laughedelic/keymap.c
@@ -37,92 +37,31 @@ enum planck_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Workman (https://workmanlayout.org)
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   D  |   R  |   W  |   B  |   J  |   F  |   U  |   P  |   ;  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   H  |   T  |   G  |   Y  |   N  |   E  |   O  |   I  |Enter |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   M  |   C  |   V  |   K  |   L  |   ,  |   .  |   /  |Shift |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Alt  | Cmd  |Lower |Bkspc |Space |Raise | Cmd  |AltGr | Ctrl |      |
- * `-----------------------------------------------------------------------------------'
- */
+/* Workman (https://workmanlayout.org) */
 [_WORKMAN] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,          KC_F,    KC_U,    KC_P,    KC_SCLN, KC_BSLS,
-    LCTL_T(KC_ESC), KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,          KC_N,    KC_E,    KC_O,    KC_I,    RCTL_T(KC_ENT),
-    KC_LSFT,        KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,          KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LSFT,        KC_LCTL, KC_LALT, KC_LCMD, LOWER,   KC_BSPC, SFT_T(KC_SPC), RAISE,   KC_RCMD, KC_RALT, KC_RCTL, KC_RSFT
+    LALT_T(KC_ESC), KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,          KC_F,    KC_U,    KC_P,    KC_SCLN, KC_BSLS,
+    LCTL_T(KC_TAB), KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,          KC_N,    KC_E,    KC_O,    KC_I,    RCTL_T(KC_ENT),
+    SFT_T(KC_TILD), KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,          KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+     LCMD(KC_TILD), RCMD(KC_LBRC), RCMD(KC_RBRC), KC_LCMD, LOWER, KC_BSPC, SFT_T(KC_SPC), RAISE, KC_RALT, RCMD(KC_LCBR), RCMD(KC_RCBR), RALT(KC_QUOT)
 ),
 
-/* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
-[_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
-),
-
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  =   |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  <<  | Vol- | Vol+ |  >>  |      |      |   -  |   +  |   {  |   }  |  '   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      | Mute | |>|| |      |      |   _  |   =  |   [  |   ]  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL,
-    _______, KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD, _______, _______, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_QUOT,
-    _______, _______, _______, KC_MUTE, KC_MPLY, _______, _______, KC_UNDS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  KC_MUTE, KC_VOLD, KC_VOLU, KC_MRWD, KC_MPLY, KC_MFFD, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX,
+  KC_LCTL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_PLUS, KC_MINS, KC_LCBR, KC_RCBR, KC_PIPE, XXXXXXX,
+  KC_LSFT,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL, KC_UNDS, KC_LBRC, KC_RBRC, KC_BSLS, XXXXXXX,
+  _______, _______, _______, KC_LCMD,   LOWER, KC_BSPC, SFT_T(KC_SPC), _______, KC_RALT, _______, _______, _______
 ),
 
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  -   |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   ←  |   ↓  |   ↑  |   →  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   -  |   =  |   [  |   ]  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DQUO,
-    _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_LCTL, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,
+  KC_LSFT, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, _______, _______, KC_LCMD, KC_LALT, KC_BSPC, SFT_T(KC_SPC), RAISE, KC_RALT, _______, _______, _______
 ),
 
-/* Adjust (Lower + Raise)
- * ,-----------------------------------------------------------------------------------.
- * |      |Qwerty|Workmn|Debug |Reset |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|      |      |      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, QWERTY,  WORKMAN, DEBUG,   RESET,   _______, _______, _______, _______,  _______, _______, _______,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______, _______, _______,
+    _______, QWERTY,  DEBUG,   RESET,   WORKMAN, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, A(KC_LEFT), A(KC_DOWN), A(KC_UP), A(KC_RIGHT), _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
